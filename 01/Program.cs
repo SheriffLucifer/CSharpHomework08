9d05373d8d846c7fb9a664f070fcf65b83939fcb
Oleg <olegashepelev@gmail.com>
2023-01-29 20:59:47 +0600

    Complete first task

---
+++ 01/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию 
+// элементы каждой строки двумерного массива.
+
+Console.Clear();
+
+int[,] array = GetArray(5, 15, 0, 100);
+PrintArray(array);
+
+OrderLines(array);
+PrintArray(array);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void OrderLines(int[,] array)
+{
+    Console.WriteLine("Упорядочивание элементов по убыванию каждой строки в массиве");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int result = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = result;
+                }
+            }
+        }
+    }
+}
